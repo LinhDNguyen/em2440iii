32090676b36b0ba9acbe1421a4870724369fbda6
Linh Nguyen <nvl1109@gmail.com>
2012-09-22 07:56:42 +0700

    Prepare resource

---
+++ bootloader/s3c2440_vivi_rel/include/compile.h
@@ -0,0 +1,6 @@
+#define UTS_VERSION "#0.1.4 ¶þ 12ÔÂ 26 21:09:32 CST 2006"
+#define VIVI_COMPILE_TIME "21:09:32"
+#define VIVI_COMPILE_BY "root"
+#define VIVI_COMPILE_HOST "localhost.localdomain"
+#define VIVI_COMPILE_DOMAIN "localdomain"
+#define VIVI_COMPILER "gcc version 2.95.3 20010315 (release)"
